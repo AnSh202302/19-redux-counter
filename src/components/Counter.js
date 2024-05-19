bd1fe791749769712c6007f91484572c0e899af9
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-05-19 21:17:50 +0200

    Attaching Payloads to Actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,66 +1,71 @@
-import { connect, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
-import { Component } from "react";
 
-// const Counter = () => {
-//   const counter = useSelector((state) => state.counter);
-//   const dispatch = useDispatch();
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
-//   const incrementHandler = () => {
-//     dispatch({ type: "increment" });
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const increaseHandler = () => {
+    dispatch({ type: "increase", payload: 5 });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={decrementHandler}>-</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={incrementHandler}>+</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
+
+// class Counter extends Component() {
+//   decrementHandler() {
+//     this.props.increment();
+//   }
+//   incrementHandler() {
+//     this.props.decrement();
+//   }
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.decrementHandler.bind(this)}>-</button>
+//           <button onClick={this.incrementHandler.bind(this)}>+</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
 //   };
-//   const decrementHandler = () => {
-//     dispatch({ type: "decrement" });
-//   };
-
-//   const toggleCounterHandler = () => {};
-
-//   return (
-//     <main className={classes.counter}>
-//       <h1>Redux Counter</h1>
-//       <div className={classes.value}>{counter}</div>
-//       <div>
-//         <button onClick={decrementHandler}>-</button>
-//         <button onClick={incrementHandler}>+</button>
-//       </div>
-//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
-//     </main>
-//   );
 // };
-
-class Counter extends Component() {
-  decrementHandler() {
-    this.props.increment();
-  }
-  incrementHandler() {
-    this.props.decrement();
-  }
-  toggleCounterHandler() {}
-
-  render() {
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div className={classes.value}>{this.props.counter}</div>
-        <div>
-          <button onClick={this.decrementHandler.bind(this)}>-</button>
-          <button onClick={this.incrementHandler.bind(this)}>+</button>
-        </div>
-        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-      </main>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    counter: state.counter,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: () => dispatch({ type: "increment" }),
-    decrement: () => dispatch({ type: "decrement" }),
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
